ac06e196cd5725c0e7c19021c1d85ec7f7fa172d
yankee05 <barkleye70@gmail.com>
2025-11-26 16:38:13 -0400

    Agregar logo personalizado también en página de login
    - Actualizar CustomLoginView para incluir dashboard_logo_url
    - El login ahora usará el mismo logo PNG que el dashboard

--- public/views.py
+++ public/views.py
@@ -6,6 +6,12 @@ class CustomLoginView(LoginView):
     template_name = "registration/login.html"
     redirect_authenticated_user = True
     next_page = reverse_lazy("dashboard:inicio")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Agregar logo URL para login
+        context["dashboard_logo_url"] = "/static/img/logo/logo.png"
+        return context
 
 
 class CustomLogoutView(LogoutView):
